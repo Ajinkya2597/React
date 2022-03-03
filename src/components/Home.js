b65a93d727acf5210f9f50a966a384831cc0fd32
Ajinkya Mhavan <54267000+Ajinkya2597@users.noreply.github.com>
2022-03-03 22:29:49 +0530

    Add files via upload

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+ 
+const home = () => {
+    return (
+       <div>
+          <h1>Home</h1>
+           <p>Ajinkya Mhavan</p>
+       </div>
+    );
+}
+ 
+export default home;
